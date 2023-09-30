a82705acf042f98e1743fec4a9ccf6b8954367ea
Kamil <moskalakamil07@gmail.com>
2023-09-30 18:08:14 +0200

    fix: admin screen

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,32 +1,27 @@
 import Head from "next/head";
-import {LoginScreen} from "@/features/admin/auth/screens/Login.Screen";
-import {GetStaticProps} from "next";
-import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { LoginScreen } from "@/features/admin/auth/screens/Login.screen";
 
-export default function Admin () {
-
-    return (
-        <>
-            <Head>
-                <title>test</title>
-            </Head>
-            <LoginScreen />
-        </>
-
-    );
-};
+export default function Admin() {
+  return (
+    <>
+      <Head>
+        <title>test</title>
+      </Head>
+      <LoginScreen />
+    </>
+  );
+}
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    if (!locale) {
-        throw new Error("locale is undefined");
-    }
+  if (!locale) {
+    throw new Error("locale is undefined");
+  }
 
-    return {
-        props: {
-            ...(await serverSideTranslations(locale, [
-                "common",
-                "auth"
-            ])),
-        },
-    };
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "auth"])),
+    },
+  };
 };
